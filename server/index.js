8980e31cf42c4a90f9e5d64896f0d2a30b61d2c6
Jake Patterson <jake@patt.me>
2020-10-23 21:11:12 +0100

    wip: use map

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const io = require('socket.io')({})
 // TODO: make persistent
-const docs = {}
+const docs = new Map()
 io.on('connection', socket => {
   const { room } = socket.handshake.query
-  socket.join(room).emit('joined', docs[room])
+  socket.join(room).emit('joined', docs.get(room))
   socket.on('update', changes => {
     socket.to(room).emit('update', changes)
   })
   socket.on('sync', doc => {
-    docs[room] = doc
+    docs.set(room, doc)
   })
 })
 io.listen(process.env.SNOWPACK_PUBLIC_WSS_PORT)
