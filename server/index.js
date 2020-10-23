bfc3ccfe587694ed35704b3535089016aab3a1ff
Jake Patterson <jake@patt.me>
2020-10-23 20:00:16 +0100

    wip: encrypt decrypt

--- server/index.js
+++ server/index.js
@@ -2,15 +2,13 @@ const io = require('socket.io')({})
 // TODO: make persistent
 const docs = {}
 io.on('connection', socket => {
-  const { room } = socket.handshake
+  const { room } = socket.handshake.query
   socket.join(room).emit('joined', docs[room])
   socket.on('update', changes => {
-    console.debug(changes)
     socket.to(room).emit('update', changes)
   })
   socket.on('sync', doc => {
-    console.debug(doc)
-    docs[room] = doc.join('\n')
+    docs[room] = doc
   })
 })
 io.listen(process.env.SNOWPACK_PUBLIC_WSS_PORT)
