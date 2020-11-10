9b3938bb228e5c75d2f68cf3f161531890677a7a
Jake Patterson <jake@patt.me>
2020-11-10 19:48:10 +0000

    feat: use preact App

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,5 @@
-// import { Persistence } from "./Persistence";
-// import { Controller } from "./Controller";
-
-// const persistence = new Persistence();
-// const controller = new Controller({ persistence });
-// const button = document.querySelector("#share") as Element;
-// button.addEventListener("click", async () => {
-//   await controller.share();
-// });
-// controller.start();
-
 import "./index.css";
-import { h, render } from "preact";
+import React, { h, render } from "preact";
 import App from "./components/App";
 
 render(<App />, document.querySelector("#app") as HTMLElement);
