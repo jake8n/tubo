e300abc279240c9dca4c4b5c6cd4be9da1b944df
Jake Patterson <jake@patt.me>
2020-11-10 19:48:10 +0000

    feat: use preact App

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,41 @@
-import { Component, h } from "preact";
-import Header from "../Header";
+import React, { Component, h } from "preact";
+import { Controller } from "../../Controller";
+import { Persistence } from "../../Persistence";
 
 export default class App extends Component {
+  private _persistence: Persistence;
+  private _controller: Controller;
+
+  constructor() {
+    super();
+    this._persistence = new Persistence();
+    this._controller = new Controller({ persistence: this._persistence });
+  }
+
+  componentDidMount() {
+    this._controller.start();
+  }
+
+  async onShare() {
+    // TODO: use preact router instead of page reload
+    await this._controller.share();
+  }
+
   render() {
-    return <Header />;
+    return (
+      <div>
+        <header>
+          <button id="share" onClick={this.onShare.bind(this)}>
+            Share
+          </button>
+        </header>
+        <div id="views">
+          <div id="view-js"></div>
+          <div id="view-html"></div>
+          <div id="view-css"></div>
+        </div>
+        <div id="frame"></div>
+      </div>
+    );
   }
 }
--- src/components/Header/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Component, h } from "preact";
-
-export default class Header extends Component {
-  render() {
-    return <header>Hello</header>;
-  }
-}
